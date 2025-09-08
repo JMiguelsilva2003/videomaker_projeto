9ccb004b88ee8cecfb0b0ea6a1ebdda05e919e9f
José Miguel da Silva <jmigueldasilva953@gmail.com>
2025-09-08 15:46:32 -0300

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -8,4 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
             menuHamburguer.classList.toggle('ativo');
         });
     }
+
+    const ctaLink = document.querySelector('.cta-link');
+    const mediaQuery = window.matchMedia('(max-width: 1000px)');
+
+    function handleMediaQuery(e) {
+        if (e.matches) {
+            // A tela é menor ou igual a 1000px
+            ctaLink.textContent = 'Quero impactar meu público';
+        } else {
+            // A tela é maior que 1000px
+            ctaLink.textContent = 'Garanta agora o seu projeto exclusivo e dê o próximo passo para se destacar.';
+        }
+    }
+
+    handleMediaQuery(mediaQuery);
+
+    mediaQuery.addEventListener('change', handleMediaQuery);
 });
